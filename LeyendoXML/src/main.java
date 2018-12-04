78bba35a6d10e2385d96f50d38c100c5d473a646
lz <luiszumarraga95@gmail.com>
2018-12-04 12:15:47 +0100

    xlm

---
+++ LeyendoXML/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        String nombreArchivo = "discos.xml";
+        PruebaSAX1 probando = new PruebaSAX1(nombreArchivo);
+        probando.andale();
+
+    }
+
+}
